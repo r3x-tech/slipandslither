04310a42bcbd3939fb76c22b546857a942736d5f
adrian-petrykiw <cucumber901@gmail.com>
2024-04-07 10:00:56 -0500

    added ora

--- utils/shyft.ts
+++ utils/shyft.ts
@@ -1,4 +1,4 @@
-import { ServiceCharge } from "@/types/types";
+import { NFT, ServiceCharge, TxResponse } from "@/types/types";
 import axios, { AxiosResponse } from "axios";
 
 export async function getUSDCBalance(walletAddress: string): Promise<any> {
@@ -26,5 +26,103 @@ export async function getUSDCBalance(walletAddress: string): Promise<any> {
   } catch (error) {
     console.error("Error fetching USDC balance:", error);
     return null;
+  }
+}
+
+export async function createNFTFromMetadata(
+  metadataUri: string,
+  receiver: string,
+  feePayer: string,
+  maxSupply?: number,
+  serviceCharge?: ServiceCharge,
+  collectionAddress?: string
+): Promise<TxResponse | null> {
+  const endpoint = "https://api.shyft.to/sol/v1/nft/create_from_metadata";
+  const apiKey = process.env.NEXT_PUBLIC_S_KEY;
+  const headers = {
+    "x-api-key": apiKey,
+    "Content-Type": "application/json",
+  };
+
+  const requestBody = {
+    network: "mainnet-beta",
+    metadata_uri: metadataUri,
+    ...(maxSupply && { max_supply: maxSupply }),
+    ...(collectionAddress && { collection_address: collectionAddress }),
+    receiver: receiver,
+    fee_payer: feePayer,
+    ...(serviceCharge && { service_charge: serviceCharge }),
+  };
+
+  try {
+    console.log("5.0");
+
+    const response = await axios.post(endpoint, requestBody, {
+      headers: headers,
+    });
+    const responseData = response.data;
+
+    if (responseData.success) {
+      console.log("5.2 - responseData: ", responseData);
+      return responseData;
+    } else {
+      console.log("5.3 - responseData: ", responseData);
+      throw Error("Error creating NFT:", responseData.message);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+}
+
+export async function fetchNFTsFromWallet(
+  walletAddress: string,
+  collectionAddresses: string[]
+): Promise<NFT[]> {
+  const endpoint = "https://api.shyft.to/sol/v2/nft/read_all";
+  const apiKey = process.env.NEXT_PUBLIC_S_KEY;
+  let allMatchingNfts: NFT[] = [];
+  let currentPage = 1;
+
+  try {
+    while (true) {
+      const params = {
+        network: "mainnet-beta",
+        address: walletAddress,
+        page: currentPage,
+      };
+
+      const response = await axios.get<any>(endpoint, {
+        headers: {
+          "x-api-key": apiKey,
+        },
+        params: params,
+      });
+
+      const fetchedNfts = response.data.result.nfts;
+      console.log("fetchedNfts: ", fetchedNfts);
+
+      console.log("fetchedNfts 0 collection: ", fetchedNfts[0].collection);
+
+      const matchingNfts = fetchedNfts.filter(
+        (nft: { collection?: { address?: string } }) =>
+          nft.collection?.address &&
+          collectionAddresses.includes(nft.collection.address)
+      );
+
+      allMatchingNfts = [...allMatchingNfts, ...matchingNfts];
+
+      if (currentPage >= response.data.result.total_pages) {
+        break;
+      }
+
+      currentPage++;
+    }
+    console.log("allMatchingNfts:", allMatchingNfts);
+
+    return allMatchingNfts;
+  } catch (error) {
+    console.error("Error fetching NFTs:", error);
+    throw error;
   }
 }
